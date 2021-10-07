42fdcbc90ce896fc722824c10efd8ad91d54b9ac
duckflew <129duckflew@gmail.com>
2021-10-08 01:37:10 +0800

    分页插件

---
+++ server/src/main/java/cn/duckflew/config/MybatisConfig.java
@@ -0,0 +1,19 @@
+package cn.duckflew.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+@MapperScan("cn.duckflew.dao")
+@Configuration
+public class MybatisConfig
+{
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
