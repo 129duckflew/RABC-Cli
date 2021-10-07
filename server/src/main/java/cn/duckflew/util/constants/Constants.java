f5eb8811b4662f76f6d347c13252a622c733ad9a
duckflew <129duckflew@gmail.com>
2021-10-08 00:39:46 +0800

    修改操作成功的信息

--- server/src/main/java/cn/duckflew/util/constants/Constants.java
+++ server/src/main/java/cn/duckflew/util/constants/Constants.java
@@ -8,5 +8,5 @@ package cn.duckflew.util.constants;
 public class Constants
 {
     public static final String SUCCESS_CODE = "200";
-    public static final String SUCCESS_MSG = "";
+    public static final String SUCCESS_MSG = "操作成功";
 }
