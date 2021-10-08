b104269c08460951a83c88eb709cad1f158e5a9e
duckflew <129duckflew@gmail.com>
2021-10-09 01:48:21 +0800

    生成所有需要用到的文件

---
+++ server/src/main/java/cn/duckflew/dao/RolePermissionMapper.java
@@ -0,0 +1,16 @@
+package cn.duckflew.dao;
+
+import cn.duckflew.pojo.RolePermission;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 角色-权限关联表 Mapper 接口
+ * </p>
+ *
+ * @author duckflew
+ * @since 2021-10-08
+ */
+public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+}
---
+++ server/src/main/java/cn/duckflew/dao/UserRoleMapper.java
@@ -0,0 +1,16 @@
+package cn.duckflew.dao;
+
+import cn.duckflew.pojo.UserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户-角色关联表 Mapper 接口
+ * </p>
+ *
+ * @author duckflew
+ * @since 2021-10-08
+ */
+public interface UserRoleMapper extends BaseMapper<UserRole> {
+
+}
